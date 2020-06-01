a04eb5bb5ecd409d29a58fb2e276810bcd0e71fe
Shimul Dey <dey.shimul09@gmail.com>
2020-06-01 06:09:30 +0600

    sign in page added

--- src/App.js
+++ src/App.js
@@ -5,13 +5,18 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component';
+
+import SigninSignup from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SigninSignup} />
       </Switch>
     </div>
   );
---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './sign-in-and-sign-up.style.scss';
+
+import Signin from '../../components/sign-in/sign-in.component';
+
+const SigninSignupPage = () => (
+    <div className='sign-in-and-sign-up'>
+        <Signin />
+    </div>
+);
+
+export default SigninSignupPage;
